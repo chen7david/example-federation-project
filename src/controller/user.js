84ce3b5ceef2ef0095301cd3434395499f4a8e0f
David <chen7david@me.com>
2020-03-21 16:43:30 +0800

    update

--- src/controller/user.js
+++ src/controller/user.js
@@ -29,18 +29,26 @@ module.exports = {
     },
 
     patch: async (req, res, next) => {
-        const { cargo } = req.tools
+        const { cargo, info } = req.tools
         const { param, body } = req.ctx
         const user = await param.user
             .$query()
             .patch(body)
             .returning('*')
         cargo.payload(user)
+        cargo.details(info('updated', 'user').render())
         res.status(200).json(cargo)
     },
 
     delete: async (req, res, next) => {
-        const { cargo } = req.tools
+        const { cargo, info, error } = req.tools
+        const { $user, param } = req.ctx
+        if(param.user.id == $user.id) return next(error('forbidden','operation'))
+        const deleted = await param.user
+            .$query()
+            .delete()
+        cargo.payload({deleted: true}) 
+        cargo.details(info('deleted', 'user').render())
         res.status(200).json(cargo)
     },
 }
